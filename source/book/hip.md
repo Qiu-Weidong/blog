71c326704b88e95e42206352fa350e67313391ba
unknown <568385476@qq.com>
2022-11-03 16:08:54 +0800

    完成了book

--- source/book/hip.md
+++ source/book/hip.md
@@ -2,9 +2,13 @@
 title: 周髀程式释注
 date: 2022-11-01 16:55:31
 type: 'book'
-aside: false
+aside: true
+textlayer: true
+url: 'https://cdn.jsdelivr.net/gh/Qiu-Weidong/pictures/pdf/周髀程式释注.pdf'
 ---
 
 来点内容
 在上方配置`aside:false`可以關閉側邊欄。
+
+<!-- textlayer 是否显示文字层 -->
 
---
+++ themes/butterfly/source/js/book.js
@@ -0,0 +1,116 @@
+
+"use strict";
+
+(function () {
+    const url = document.currentScript.getAttribute('url');
+    if (!url || url === '') return;
+    if (!pdfjsLib ||/* !pdfjsViewer */ !pdfjsLib.getDocument) {
+        return alert('Please build the pdfjs-dist library using\n  `gulp dist-install`');
+    }
+
+    pdfjsLib.GlobalWorkerOptions.workerSrc =
+        "https://cdn.jsdelivr.net/gh/Qiu-Weidong/rain/resources/pdfjs-dist/build/pdf.worker.min.js";
+
+    let pdfDoc = null,
+        pageNum = 1,
+        pageRendering = false,
+        pageNumPending = null,
+        scale = 2.0,
+        canvas = document.getElementById('book-canvas'),
+        ctx = canvas.getContext('2d');
+
+    /**
+     * Get page info from document, resize canvas accordingly, and render page.
+     * @param num Page number.
+     */
+    function renderPage(num) {
+        pageRendering = true;
+        // Using promise to fetch the page
+        pdfDoc.getPage(num).then(function (page) {
+            var viewport = page.getViewport({ scale: scale, });
+            // Support HiDPI-screens.
+            var outputScale = window.devicePixelRatio || 1;
+
+            canvas.width = Math.floor(viewport.width * outputScale);
+            canvas.height = Math.floor(viewport.height * outputScale);
+            canvas.style.width = "100%";
+
+            var transform = outputScale !== 1
+                ? [outputScale, 0, 0, outputScale, 0, 0]
+                : null;
+
+            // Render PDF page into canvas context
+            var renderContext = {
+                canvasContext: ctx,
+                transform: transform,
+                viewport: viewport,
+            };
+            var renderTask = page.render(renderContext);
+
+            // Wait for rendering to finish
+            renderTask.promise.then(function () {
+                pageRendering = false;
+                if (pageNumPending !== null) {
+                    // New page rendering is pending
+                    renderPage(pageNumPending);
+                    pageNumPending = null;
+                }
+            });
+        });
+    }
+
+    /**
+     * If another page rendering in progress, waits until the rendering is
+     * finished. Otherwise, executes rendering immediately.
+     */
+    function queueRenderPage(num) {
+        if (pageRendering) {
+            pageNumPending = num;
+        } else {
+            renderPage(num);
+        }
+    }
+
+    /**
+     * Displays previous page.
+     */
+    function onPrevPage() {
+        if (pageNum <= 1) {
+            return;
+        }
+        pageNum--;
+        queueRenderPage(pageNum);
+    }
+    // document.getElementById('prev').addEventListener('click', onPrevPage);
+
+    /**
+     * Displays next page.
+     */
+    function onNextPage() {
+        if (pageNum >= pdfDoc.numPages) {
+            return;
+        }
+        pageNum++;
+        queueRenderPage(pageNum);
+    }
+    // document.getElementById('next').addEventListener('click', onNextPage);
+    canvas.addEventListener('click', onNextPage);
+    /**
+     * Asynchronously downloads PDF.
+     */
+    const CMAP_URL = "https://cdn.jsdelivr.net/gh/Qiu-Weidong/rain/resources/pdfjs-dist/cmaps/";
+    const CMAP_PACKED = true;
+    const ENABLE_XFA = true;
+    var loadingTask = pdfjsLib.getDocument({
+        url,
+        cMapUrl: CMAP_URL,
+        cMapPacked: CMAP_PACKED,
+        enableXfa: ENABLE_XFA,
+    });
+    loadingTask.promise.then(function (pdfDoc_) {
+        pdfDoc = pdfDoc_;
+        renderPage(pageNum);
+    });
+})();
+
+
